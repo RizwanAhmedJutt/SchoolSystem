708272e904756007766ce5ad7c84d4538d1371c9
RizwanAhmedJutt <rizwan6768@gmail.com>
2016-04-09 17:12:36 +0500

    fix Closing  braces bug.

--- SMSDAL/DAL/DailyAssessmentTypeDAO.cs
+++ SMSDAL/DAL/DailyAssessmentTypeDAO.cs
@@ -30,7 +30,7 @@ namespace SMSDAL.DAL
                     gObjDatabase.AddInParameter(objDbCommand, "@CreatedById", DbType.String, dAssessmentType.CreatedById);
                     gObjDatabase.AddInParameter(objDbCommand, "@CreatedDate", DbType.DateTime, dAssessmentType.CreateDate);
                     gObjDatabase.AddInParameter(objDbCommand, "@ModifiedById", DbType.String, string.IsNullOrEmpty(dAssessmentType.ModifiedById)?(object)dAssessmentType.ModifiedById:dAssessmentType.ModifiedById);
-                    gObjDatabase.AddInParameter(objDbCommand, "@ModifiedDate", DbType.DateTime, dAssessmentType.ModifiedDate==null?(object)DBNull.Value:dAssessmentType.ModifiedDate;
+                    gObjDatabase.AddInParameter(objDbCommand, "@ModifiedDate", DbType.DateTime, dAssessmentType.ModifiedDate==null?(object)DBNull.Value:dAssessmentType.ModifiedDate);
                     gObjDatabase.AddOutParameter(objDbCommand, "@AssessmentTypenewId", DbType.Int32, 4);
                     SqlParameter returnParameter = new SqlParameter("RetValue", SqlDbType.Int);
                     returnParameter.Direction = ParameterDirection.ReturnValue;
