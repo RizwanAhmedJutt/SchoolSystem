01391d0be95a0c7d49b170093b0ef136ed060c4f
RizwanAhmedJutt <rizwan6768@gmail.com>
2016-05-21 12:48:04 +0500

    Student Export File Changes..

--- SMSDataContract/Accounts/StudentDetail.cs
+++ SMSDataContract/Accounts/StudentDetail.cs
@@ -9,6 +9,16 @@ namespace SMSDataContract.Accounts
 {
    public class StudentDetail
     {
+        public StudentDetail()
+        {
+            this.Student = new Student();
+            this.StudentAddress = new StudentAddress();
+            this.GuardianContact = new GuardianContacts();
+            this.GuardianDetail = new GuardianDetail();
+            this.Cities = new City();
+            this.AdmissionGranted = new AdmissionGranted();
+            this.AcadmicClass = new AcadmicClass();
+        }
         public Student Student { get; set; }
         public StudentAddress StudentAddress { get; set; }
         public GuardianContacts GuardianContact { get; set; }
