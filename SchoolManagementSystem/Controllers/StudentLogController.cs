5d025ebe55a4febeef5974bde699a76f2ddd8e20
RizwanAhmedJutt <rizwan6768@gmail.com>
2016-07-30 12:49:18 +0500

    Add New layout and student reports

---
+++ SchoolManagementSystem/Controllers/StudentLogController.cs
@@ -0,0 +1,29 @@
+﻿using SMSBusiness.Repository.Abstract;
+using SMSBusiness.Repository.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SchoolManagementSystem.Controllers
+{
+    public class StudentLogController : Controller
+    {
+        IAssessmentCategories repoAssessmentCategory = new AssessmentCategoriesBLL();
+        IDailyAssessmentType repoAssessmentType = new DailyAssessmentTypeBLL();
+        IDailyAssessmentSubType repoAssessementSubType = new DailyAssessmentSubTypeBLL();
+        IDailyAssessmentOperation repoOperation = new DailyAssessmentOperationBLL();
+        IAcadmicAssessmentOperation repAcOperation = new AcadmicAssessmentOperationBLL();
+        ITeacherAssessmentOperation repTAOperation = new TeacherAssessmentOperationBLL();
+        public ActionResult StudentGeneralAssessment(int? AcadmicClassId, int? StudentId, string CreateDate)
+        {
+
+            return View(repoAssessementSubType.GetStudentGeneralAssessment(AcadmicClassId, StudentId, CreateDate).ToList());
+        }
+        public ActionResult StudentAcadmicAssessment(int? AcadmicClassId, int? StudentId, int? CourseId, string CreateDate)
+        {
+            return View(repoAssessementSubType.GetStudentAcadmicAssessment(AcadmicClassId, StudentId, CourseId, CreateDate).ToList());
+        }
+	}
+}
