1c634c09f5f0b156253ea166fc26b4e1af56e800
RizwanAhmedJutt <rizwan6768@gmail.com>
2016-07-30 15:19:02 +0500

    add new Views..

--- SchoolManagementSystem/Controllers/StudentLogController.cs
+++ SchoolManagementSystem/Controllers/StudentLogController.cs
@@ -1,8 +1,10 @@
 ﻿using SMSBusiness.Repository.Abstract;
 using SMSBusiness.Repository.Concrete;
+using SMSDataContract.Common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -16,14 +18,29 @@ namespace SchoolManagementSystem.Controllers
         IDailyAssessmentOperation repoOperation = new DailyAssessmentOperationBLL();
         IAcadmicAssessmentOperation repAcOperation = new AcadmicAssessmentOperationBLL();
         ITeacherAssessmentOperation repTAOperation = new TeacherAssessmentOperationBLL();
-        public ActionResult StudentGeneralAssessment(int? AcadmicClassId, int? StudentId, DateTime CreateDate)
+      
+        public ActionResult GetStudentReport(int? StudentId, int? AcadmicClassId, string Month)
         {
-            
-            return View(repoAssessementSubType.GetStudentGeneralAssessment(AcadmicClassId, StudentId, CreateDate).ToList());
-        }
-        public ActionResult StudentAcadmicAssessment(int? AcadmicClassId, int? StudentId, int? CourseId, string CreateDate)
-        {
-            return View(repoAssessementSubType.GetStudentAcadmicAssessment(AcadmicClassId, StudentId, CourseId, CreateDate).ToList());
+            StringBuilder courseIDs = new StringBuilder();
+            StudentMonthReportHelpers smrh = new StudentMonthReportHelpers();
+            if (AcadmicClassId > 0)
+            {
+
+                smrh.Courses = repAcOperation.GetStudentAssessmentCourse(StudentId, AcadmicClassId, Month);
+                // Get CourseId to fetch only specific courses data.
+                courseIDs = repAcOperation.StudentAssessmentCourseIDs(StudentId, AcadmicClassId, Month);
+                // confirm course must exist against assessment
+                if (!string.IsNullOrEmpty(courseIDs.ToString()))
+                {
+                    smrh.AcadmicAssessment = repAcOperation.GetStudentAssessmentByCourses(StudentId, AcadmicClassId, Month, courseIDs);
+                    smrh.GeneralAssessment = repAcOperation.GetStudentGeneralAssessmentResult(StudentId, AcadmicClassId, Month);
+                }
+                return View(smrh);
+            }
+            else
+            {
+                return View(smrh);
+            }
         }
 	}
 }
