8401fa57ac9d88487a6128ce1f4cbc1eea766062
RizwanAhmedJutt <rizwan6768@gmail.com>
2016-10-18 17:12:18 +0500

    add delete method signature..

--- SMSBusiness/Repository/Abstract/IDailyAssessmentSubType.cs
+++ SMSBusiness/Repository/Abstract/IDailyAssessmentSubType.cs
@@ -14,6 +14,9 @@ namespace SMSBusiness.Repository.Abstract
         List<DailyAssessmentSubType> GetALLTeacherGeneralAssessmentSubType();
         DailyAssessmentSubType GetDailyAssessmentSubTypeById(int AssessmentSubTypeId);
         int AddChangesAssessmentSubType(DailyAssessmentSubType dAssessmentsubType);
+        int DeleteStudentGeneralAssessment(int AcadmicClassId, int StudentId, DateTime CreateDate);
+        int DeleteStudentAcadmicAssessment(int AcadmicClassId, int StudentId, int CourseId, string CreateDate);
+        int DeleteTeacherAssessment(int AcadmicClassId, int TeacherId, int CourseId, string CreateDate);
         DailyAssessmentSubType CheckSubAssementExist(int ParentAssessmentId, string SubAssessmentName);
         List<DailyAssessmentSubType> GetStudentGeneralAssessment(int? AcadmicClassId, int? StudentId, DateTime CreateDate);
         List<DailyAssessmentSubType> GetStudentAcadmicAssessment(int? AcadmicClassId, int? StudentId, int? CourseId, string CreateDate);
